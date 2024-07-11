930ffceb0cf3c7f335a5e8317f826a516c3eb910
Amy Li <amy_li_yuqing@hotmail.com>
2024-07-11 04:53:36 -0400

    fix(remove react router): remove react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './App.css';
 import DynamicBackground from './components/DynamicBackground';
 import AboutPage from './pages/AboutPage';
-import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="flex-1">
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-
-        {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <AboutPage />
     </div>
   );
 }
@@ -27,7 +15,7 @@ function NoMatch() {
   return (
     <DynamicBackground>
       <div className="main">
-        <h2 className="font-roboto font-semibold text-3xl text-shadow-lg">
+        <h2 className="text-3xl font-semibold font-roboto text-shadow-lg">
           Nothing to see here!
         </h2>
         <p>
